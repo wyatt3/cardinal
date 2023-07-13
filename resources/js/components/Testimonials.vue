88b460ca004909c5b42fecff6b8defc3be3dab22
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-07-13 10:24:04 -0600

    Make testimonials dynamic, need to hook up to google

--- resources/js/components/Testimonials.vue
+++ resources/js/components/Testimonials.vue
@@ -6,7 +6,11 @@
         <p class="lead mb-0">Our customers love working with us</p>
       </div>
       <div class="row gx-5 justify-content-center">
-        <div class="col-lg-6">
+        <div
+          class="col-lg-6"
+          v-for="testimonial in testimonials"
+          :key="testimonial.index"
+        >
           <!-- Testimonial 1-->
           <div class="card mb-4">
             <div class="card-body p-4">
@@ -25,24 +29,6 @@
               </div>
             </div>
           </div>
-          <!-- Testimonial 2-->
-          <div class="card">
-            <div class="card-body p-4">
-              <div class="d-flex">
-                <div class="flex-shrink-0">
-                  <i class="bi bi-chat-right-quote-fill text-primary fs-1"></i>
-                </div>
-                <div class="ms-4">
-                  <p class="mb-1">
-                    The whole team was a huge help with putting things together
-                    for our company and brand. We will be hiring them again in
-                    the near future for additional work!
-                  </p>
-                  <div class="small text-muted">- Client Name, Location</div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -50,7 +36,33 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Testimonials",
+  data() {
+    return {
+      testimonials: [
+        {
+          index: 1,
+          name: "Client Name",
+          location: "Location",
+          text: "Thank you for putting together such a great product. We loved working with you and the whole team, and we will be recommending you to others!",
+        },
+        {
+          index: 2,
+          name: "Client Name",
+          location: "Location",
+          text: "Thank you for putting together such a great product. We loved working with you and the whole team, and we will be recommending you to others!",
+        },
+        {
+          index: 3,
+          name: "Client Name",
+          location: "Location",
+          text: "Thank you for putting together such a great product. We loved working with you and the whole team, and we will be recommending you to others!",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
