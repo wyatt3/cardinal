ed6086954102ae3d11ce12d9faa99e395da5ee3f
Wyatt Johnson <wyatt.j1834@gmail.com>
2026-06-16 20:41:16 -0600

    remove admin routes directive

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,6 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @routes
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
